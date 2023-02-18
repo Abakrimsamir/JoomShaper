65515dea93a390a686bd613275fe5dd6c5d613d2
Samir Abakrim <123333480+Abakrimsamir@users.noreply.github.com>
2023-02-18 19:37:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JoomShaper
+ Center Atlantique Project for Courses website Front-end 
---
+++ index.js
@@ -0,0 +1,45 @@
+var a= 21
+var user= {
+    nom :"samir",
+    age : 21,
+    bac : true,
+}
+//  var car = [
+//     {nom:"DACIA", model:"2011", country:"Morocco"},
+//     {nom:"BMW", model:"2018", country:"Germany"},
+//     {nom:"Porche GT3", model:"2021", country:"America"},
+//     {nom:"Lamborghini", model:"2015", country:"Italy"},
+
+//  ]
+
+ let car = []
+// console.log(typeof car);
+
+// var a = 1; var b = 2
+
+
+// console.log(a+b);
+
+// if(b == 0){
+//     console.log("division by 0");
+// }
+// if(b > 0){
+  
+//     var c = a / b
+//     console.log(c);
+// }
+
+var user = {
+    nom : "ahmed",
+    age : 17,
+} 
+
+if(user.age > 18 ){
+    console.log("you are welcomsse");
+}
+else if(user.age == 18 ){
+    console.log("you are welcome");
+}
+else{
+    console.log("you are not allowed");
+}
